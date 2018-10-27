9aa843417a6b76dd5699a5bbcc7be8433da28025
Corina Talavera <corina.talavera@gmail.com>
2018-10-27 00:40:54 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,9 @@
 def square_array(array)
-number.collect do |i|
-    square = i*i
+square_array(numbers)
+# => [1,4,9])
+ 
+new_numbers = [9,10,16,25]
+ 
+square_array(new_numbers)
+# => [81,100,256,625]
 end
