64a7cd2b6e56d2f47040fc701ee0c40a659fc07b
Corina Talavera <corina.talavera@gmail.com>
2018-10-27 00:45:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+numbers = [1,2,3]
+numbers.each do |num|
+    new_numbers << (num ** 2)
+ 
+square_array(numbers)
+# => [1,4,9])
+ 
+new_numbers = [9,10,16,25]
+ new_numbers.square! 
+square_array(new_numbers)
+# => [81,100,256,625]
 end
